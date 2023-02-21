41f57f842a0b7ceb0b2e9759dba1464e2a244c7e
WebHawky <budzyk17@gmail.com>
2023-02-21 12:04:31 +0200

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,63 +2,103 @@ import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import { Form, Filter, ContactList } from './index';
+
+import initialContacts from '../contacts.json';
 import s from './app.module.scss';
 
-const PARSED_DATA = JSON.parse(localStorage.getItem('contacts'));
+// const PARSED_DATA = JSON.parse(localStorage.getItem('contacts')) ?? [];
+
+// export default function App() {
+//   const [contacts, setContacts] = useState(PARSED_DATA);
+//   const [filter, setFilter] = useState('');
+
+//   const addNewContact = data => {
+//     const newItem = {
+//       id: nanoid(),
+//       name: data.name,
+//       number: data.number,
+//     };
+
+//     const contactExist = contacts.some(
+//       ({ name, number }) => name === newItem.name || number === newItem.number
+//     );
+//     if (contactExist) {
+//       alert(`${newItem.name} is already exist in your contact list`);
+//     } else {
+//       setContacts(prevState => [...prevState, newItem]);
+//     }
+//   };
+
+//   const onHandleFilter = e => {
+//     setFilter(e.target.value);
+//   };
+
+//   const onHandleVisibleContacts = () => {
+//     return contacts.filter(contact => {
+//       return contact.name.toLowerCase().includes(filter.toLowerCase());
+//     });
+//   };
+
+//   const onHandleDelete = contactId => {
+//     setContacts(contacts.filter(contact => contact.id !== contactId));
+//   };
+
+//   useEffect(() => {
+//     localStorage.setItem('contacts', JSON.stringify(contacts));
+//   }, [contacts]);
+
+//   return (
+//     <div className={s.app}>
+//       <h2>Phonebook</h2>
+//       <Form onSubmit={addNewContact} />
+//       <h2>Contacts</h2>
+//       <Filter filterEl={filter} onChange={onHandleFilter} />
+//       <ContactList
+//         contacts={onHandleVisibleContacts()}
+//         onDeleteContact={onHandleDelete}
+//       />
+//     </div>
+//   );
+// }
 
 export default function App() {
-  const [contacts, setContacts] = useState(PARSED_DATA);
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) || initialContacts
+  );
   const [filter, setFilter] = useState('');
 
-  const addNewContact = data => {
-    const newItem = {
-      id: nanoid(),
-      name: data.name,
-      number: data.number,
-    };
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-    const contactExist = contacts.some(
-      ({ name, number }) => name === newItem.name || number === newItem.number
-    );
-    if (contactExist) {
-      alert(`${newItem.name} is already exist in your contact list`);
+  const addNewContact = contact => {
+    if (contacts.find(({ name }) => name === contact.name)) {
+      alert(`${contact.name} is already exist in your contact list`);
     } else {
-      setContacts(prevState => [...prevState, newItem]);
+      setContacts(state => [{ ...contact, id: nanoid() }, ...contacts]);
     }
-  };
-
-  const onHandleFilter = e => {
-    setFilter(e.target.value);
-  };
-
-  const onHandleVisibleContacts = () => {
-    return contacts.filter(contact => {
-      return contact.name.toLowerCase().includes(filter.toLowerCase());
-    });
   };
 
   const onHandleDelete = contactId => {
     setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
-  useEffect(() => {
-    if (!PARSED_DATA) {
-      setContacts(PARSED_DATA);
-    }
-  }, []);
+  const onHandleFilter = e => {
+    setFilter(e.target.value);
+  };
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const onHandleVisibleContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(filter)
+  );
 
   return (
     <div className={s.app}>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
       <Form onSubmit={addNewContact} />
       <h2>Contacts</h2>
-      <Filter filterEl={filter} onChange={onHandleFilter} />
+      <Filter onChange={onHandleFilter} />
       <ContactList
-        contacts={onHandleVisibleContacts()}
+        contacts={onHandleVisibleContacts}
         onDeleteContact={onHandleDelete}
       />
     </div>
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,21 +2,15 @@ import PropTypes from 'prop-types';
 
 import s from './filter.module.scss';
 
-export default function Filter({ filterEl, onChange }) {
+export default function Filter({ onChange }) {
   return (
     <div className={s.container}>
       <label className={s.title}>Filter by name</label>
-      <input
-        type="text"
-        value={filterEl}
-        onChange={onChange}
-        className={s.input_filter}
-      ></input>
+      <input type="text" onChange={onChange} className={s.input_filter}></input>
     </div>
   );
 }
 
 Filter.propTypes = {
-  filterEl: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
