f06fa9b68e8d424dbcf40755538c474d6d96b005
WebHawky <budzyk17@gmail.com>
2023-02-21 12:10:46 +0200

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,61 +6,6 @@ import { Form, Filter, ContactList } from './index';
 import initialContacts from '../contacts.json';
 import s from './app.module.scss';
 
-// const PARSED_DATA = JSON.parse(localStorage.getItem('contacts')) ?? [];
-
-// export default function App() {
-//   const [contacts, setContacts] = useState(PARSED_DATA);
-//   const [filter, setFilter] = useState('');
-
-//   const addNewContact = data => {
-//     const newItem = {
-//       id: nanoid(),
-//       name: data.name,
-//       number: data.number,
-//     };
-
-//     const contactExist = contacts.some(
-//       ({ name, number }) => name === newItem.name || number === newItem.number
-//     );
-//     if (contactExist) {
-//       alert(`${newItem.name} is already exist in your contact list`);
-//     } else {
-//       setContacts(prevState => [...prevState, newItem]);
-//     }
-//   };
-
-//   const onHandleFilter = e => {
-//     setFilter(e.target.value);
-//   };
-
-//   const onHandleVisibleContacts = () => {
-//     return contacts.filter(contact => {
-//       return contact.name.toLowerCase().includes(filter.toLowerCase());
-//     });
-//   };
-
-//   const onHandleDelete = contactId => {
-//     setContacts(contacts.filter(contact => contact.id !== contactId));
-//   };
-
-//   useEffect(() => {
-//     localStorage.setItem('contacts', JSON.stringify(contacts));
-//   }, [contacts]);
-
-//   return (
-//     <div className={s.app}>
-//       <h2>Phonebook</h2>
-//       <Form onSubmit={addNewContact} />
-//       <h2>Contacts</h2>
-//       <Filter filterEl={filter} onChange={onHandleFilter} />
-//       <ContactList
-//         contacts={onHandleVisibleContacts()}
-//         onDeleteContact={onHandleDelete}
-//       />
-//     </div>
-//   );
-// }
-
 export default function App() {
   const [contacts, setContacts] = useState(
     () => JSON.parse(localStorage.getItem('contacts')) || initialContacts
